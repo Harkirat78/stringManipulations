4dae9eaa2713f5da45b89142355c639e38b589ed
Harkirat Soomal <hsoomal@fred.socs.uoguelph.ca>
2023-03-04 15:12:20 -0500

    updated

--- include/soomalHarkiratA2.h
+++ include/soomalHarkiratA2.h
@@ -1,3 +1,19 @@
+/*
+Student Name: Harkirat Soomal
+Student ID: 1232181
+Due Date: March 3, 2023
+Course: CIS*2500
+I have exclusive control over this submission via my password.
+By including this header comment, I certify that:
+1) I have read and understood the policy on academic integrity.
+2) I have completed Moodle's module on academic integrity.
+3) I have achieved at least 80% on the academic integrity quiz
+I assert that this work is my own. I have appropriate acknowledged
+any and all material that I have used, be it directly quoted or
+paraphrased. Furthermore, I certify that this assignment was written
+by me in its entirety.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,3 +27,11 @@ void dejaVu (char * aString, int * numWords, int * numSentence);
 //function 3
 char * goBig(char * aString);
 
+//function 4
+char ** goAway(char *aString, int *numWordsReturned);
+
+//function 5
+char * breathless (char * aString);
+
+//function 6
+void tail(char *aString, int whichWord, char *desiredSuffix);
