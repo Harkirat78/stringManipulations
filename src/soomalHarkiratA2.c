1a7cee32be7c42b48b8d9ea49269c6b6d1607308
Harkirat Soomal <hsoomal@fred.socs.uoguelph.ca>
2023-03-06 19:59:26 -0500

    update

--- src/soomalHarkiratA2.c
+++ src/soomalHarkiratA2.c
@@ -57,7 +57,7 @@ Function name: dejaVu
 Input: string pointer, two int pointers
 Returns: void
 */
-void dejaVu(char *aString, int *numWords, int *numSentences) {
+void dejaVu(char *aString, int *numWords, int *numSentences){
 
     int numChars = 0; //keeps track of number of chars in the current word that are being proccessed
     //initialize both to 0 to ensure file read from beginning
@@ -65,18 +65,18 @@ void dejaVu(char *aString, int *numWords, int *numSentences) {
     *numSentences = 0;
 
     //loop will run till the string ends
-    for (int i = 0; aString[i] != '\0'; i++) {
+    for (int i = 0; aString[i] != '\0'; i++){
         char current = aString[i]; //char variable willused to store the current char value within string
         numChars++; //increment the number of characters
 
         //check for end of the sentence which can be a those punctuations
-        if (current == '.' || current == ':' || current == ';' || current == '?' || current == '!') {
+        if (current == '.' || current == ':' || current == ';' || current == '?' || current == '!'){
             (*numSentences)++; //add 1 to the number of sentences
         }
         //check for whitespace
-        if (current == ' ' || current == '\t' || current == '\n' || current == '\r') {
+        if (current == ' ' || current == '\t' || current == '\n' || current == '\r'){
             //if there are non-space characters before this whitespace, it's the end of a word
-            if (numChars > 1) {
+            if (numChars > 1){
                 (*numWords)++; //add 1 to the number of words in string
             }
             //reset the number of characters
@@ -84,7 +84,7 @@ void dejaVu(char *aString, int *numWords, int *numSentences) {
         }
     }
     //if there are non-space characters after the last whitespace, it's the end of a word
-    if (numChars > 0) {
+    if (numChars > 0){
         (*numWords)++; //add 1 to the number of words in string
     }
 }
@@ -100,7 +100,7 @@ Function name: goBig
 Inputs: string pointer
 Returns: char *
 */
-char * goBig(char * aString) {
+char * goBig(char * aString){
     
     //determine the length of the input string
     int length = strlen(aString)-1; //subtract 1 for the NULL character
@@ -113,19 +113,19 @@ char * goBig(char * aString) {
     printf("Enter %d positive integers separated by spaces: ", length);
    
     //loop will iterate for the length of the string provided
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++){
         int num = 0;
         //keep asking for input until a positive integer is provided
         int validInput = 0; //variable to check if input is valid or invalid(<=0)
         //loop will iterate till input is >0
-        while (validInput==0) {
+        while (validInput==0){
             scanf("%d", &num); 
             //check if the input is a valid positive integer
-            if (num > 0) {
+            if (num > 0){
                 validInput = 1;
             }
             //display an invalid input message
-            else {
+            else{
                 printf("Invalid input. Please enter a positive integer.\n");
             }
         }
@@ -133,7 +133,7 @@ char * goBig(char * aString) {
         stretched = realloc(stretched, sizeof(char) * (strlen(stretched) + num + 1)); //num+1 is added as num represents amount each string in original aString must be repeated
 
         //append the letters from aString to the stretched string, each repeated num times
-        for (int j = 0; j < num; j++) {
+        for (int j = 0; j < num; j++){
             strncat(stretched, &aString[i], 1); //function will append a single character from aString to end of strecthed string one at a time
         }
     }
@@ -149,70 +149,63 @@ Function name: goAway
 Inputs: string pointer, int pointer
 Returns: char **
 */
-char ** goAway(char * aString, int * numWordsReturned) {
+char ** goAway (char * aString, int * numWordsReturned){
 
-    //first to count the number of words in the string
-    int numWords = 0; //variable will keep track of how many words are in the dynamic string
-    int inWord = 0; //variable will if the current character is part of the word or not as if a whitespace is encountered it will set to 0
-   
-    //loop will iterate till the end of the string is reached
-    for (int i = 0; aString[i] != '\0'; i++) {
-        //check for a space, tab, newline, which indicate the end of a word
-        if (aString[i] == ' ' || aString[i] == '\n' || aString[i] == '\r' || aString[i] == '\t') {
-            //if condition is true then there is whitespace character 
-            inWord = 0;
-        }
-        //if a non-white whitespace is not encountered
-        else{
-            //check if a non-whitespace character is encountered and increase number of words by 1 
-            if (inWord==0) {
-                numWords++;
-            }
-            inWord = 1; //change inWord to 1 as the character is no longer part of the word
-        }
-    }
-    //allocate memory for the array of words and the words themselves
-    char ** words; //dynamically allocate an array of pointers to char
-    words = malloc(sizeof(char*)*numWords); //allocate for 2d dynamic string
-    int index = 0; //index of current word being processed
-    inWord = 0; //reset inWord
-    int wordStart = 0; //starting index of current word being processed
-    
-    //loop will iterate till end of string
-    for (int i = 0; aString[i] != '\0'; i++) {
-        //check for a space, tab, newline, which indicate the end of a word
-        if (aString[i] == ' ' || aString[i] == '\n' || aString[i] == '\r' || aString[i] == '\t') {
-            //encountered a whitespace character
-            if (inWord>0) {
-                //the word has been finished
-                int wordEnd = i - 1; //set the ending index of current word
-                int wordLength = wordEnd - wordStart + 1; //compute length of the word, wordEnd is last char in word and wordStart is first,add 1 to difference to include last char at the index
-                char * word = malloc(sizeof(char*)*(wordLength + 1)); //dynamically allocate a string for the word
-                //loop will iterate for the length of the particular word
-                for (int j = 0; j < wordLength; j++) {
-                    word[j] = aString[wordStart + j]; // copy the characters of the word
+   char ** wordAmount; //this char pointer points to an array of strings that have words in the input strings
+   int length; //pointer to number of words found in input string set as 0
+   *numWordsReturned = 0; //variable used to store length of each word being processed
+
+   //loop will iterate through every character in given input string
+   for(int i = 0; i < strlen(aString); i++){
+       //check if curret character is a space or newline
+       if(aString[i] == '\n' || aString[i] == ' ' || i == strlen(aString)-1){
+           //check if there are any consecutive spaces or newlines and if true go back to outer if statement
+           if(aString[i-1] == '\n' || aString[i-1] == ' '){
+           }
+           else{
+               //allocate memory for words array based on number of words found
+               if(*numWordsReturned == 0){
+                   wordAmount = calloc(1, sizeof(char*)); //initiallize words to 1 single char
+               }
+               //if words returned is not 0 reallocate enough new memory
+               else {
+                   wordAmount = realloc(wordAmount, sizeof(char*)*(*numWordsReturned+1)); //reallocate for 2d string
+               }
+               length = 0; //set length back to 0 because the word has been fully comprehended
+
+               //iterate over the characters of the string, starting from i - 1 and moving backwards until the beginning of the string (index 0)
+               for (int j = i - 1; j >= 0; j--) {
+                    //if the current character is a space or a newline character, it means we have reached the beginning of the word
+                    if (aString[j] == ' ' || aString[j] == '\n') {
+                        //we break out of the loop because we have found the beginning of the word
+                        break;
+                    } else {
+                        //if the current character is not a space or a newline character, we increment the length of the word
+                        length++;
+                    }
                 }
-                word[wordLength] = '\0'; //add the null terminator to the word
-                words[index] = word; //store the word in the array of words
-                index++; // move to the next word
-            }
-            inWord = 0; //reset inWord again
-        } else {
-            //we have encountered a non-whitespace character
-            if (inWord==0) {
-                wordStart = i; //set the starting index of current word
-            }
-            inWord = 1; //set inWord to 1 (
+                //if the current character is the end of the string, add one more character to length.
+                if(i == strlen(aString)-1){ 
+                    length++;
+                }
+                //allocate memory for the current word and add it to the words array
+                wordAmount[*numWordsReturned] = calloc(length, sizeof(char)); 
 
+                //loop will iterate for total length
+                for(int j = 0; j < length; j++){ 
+                    //check if index is the same
+                    if(i == strlen(aString)-1){ //check if the current character is the last character of the input string
+                        wordAmount[*numWordsReturned][length-j-1] = aString[i-j]; //copy the current character to the last position of the allocated memory block
+                    }
+                    else{
+                        wordAmount[*numWordsReturned][length-j-1] = aString[i-j-1]; //copy the current character to the current position in the allocated memory block
+                    }
+                }
+                *numWordsReturned = *numWordsReturned + 1; //increment the number of words found
+            }
         }
     }
-    //iterate through each element in the words array
-    for (int i = 0; i < numWords; i++) {
-        free(words[i]); //free the memory allocated for the string 
-    }
-    free(words); //free the memory allocated for the words array itself
-    *numWordsReturned = numWords; //set the output parameter so pointer points to numWords
-    return words; //return the array of words
+    return wordAmount; //return the array of words
 }
 /*
 function 5
@@ -224,16 +217,16 @@ Function name: breathless
 Inputs: string pointer
 Returns: char *
 */
-char * breathless(char * aString) {
+char * breathless(char * aString){
 
     //initialize two pointers p and q to point at beginning of the input string
     char * p = aString; //iterate over all characters in the string
     char * q = aString; //will be used to point to modified version of string that lacks punctuations
     
     //loop will iterate till end of string
-    while (*p != '\0') {
+    while (*p != '\0'){
         //check for all non-punctuations in the input string and make q point to p if true
-        if (*p != '.' && *p != ':' && *p != ';' && *p != '?' && *p != '!') {
+        if (*p != '.' && *p != ':' && *p != ';' && *p != '?' && *p != '!'){
             *q = *p; //the pointer q points to current positon in output string where the next non-puncuation character is copied
             q++; //point to next position
         }
@@ -254,38 +247,38 @@ Function name: tail
 Inputs: 2 string pointers, int
 Returns: void
 */
-void tail(char * aString, int whichWord, char * desiredSuffix) {
+void tail(char * aString, int whichWord, char * desiredSuffix){
 
     int wordCount = 1; //variable will be a counter for the current word, start at 1 since first word doesnt need space at beginning
     char *p = aString; //pointer to beginning of string
     char *start = NULL; //pointer to the start of the current word
 
     //loop will iterate till end of string and examine each char 
-    while (*p != '\0') {
+    while (*p != '\0'){
         //check if current char is a space or period
-        if (*p == ' ' || *p == '.') {
+        if (*p == ' ' || *p == '.'){
             wordCount++; //increment word 
             //if this is the target word that we are searching for, mark the start position
-            if (wordCount == whichWord) {
+            if (wordCount == whichWord){
                 start = p + 1; //point to first char of that word
             }
             //if word is before target word, calculate the length of the target and add the desired suffix
-            else if (wordCount == whichWord + 1) {
+            else if (wordCount == whichWord + 1){
                 //computer length of target word
                 int wordLen; //will calculate length of target word
-                if (*(p-1) == '.') { //if last character is a period, subtract 1 from word length
+                if (*(p-1) == '.'){ //if last character is a period, subtract 1 from word length
                     wordLen = p - start - 1; //subtract the two pointers to get length since p points to the end and start points to first character
                 }
                 else {
                     wordLen = p - start; //subtract the two pointers to get length since p points to the end and start points to first character
                 }
                 //iterate through each character in the target word
-                for (int i = 0; i < wordLen; i++) {
+                for (int i = 0; i < wordLen; i++){
                     //putchar function is used to output each character of target word and is easier to use than mutliple printf
                     putchar(*start);
                     start++; //move to next position
                     //iterates length of inputted desiredSuffix times and outputs the desired suffix after the target word
-                    for (int j = 0; j < strlen(desiredSuffix); j++) {
+                    for (int j = 0; j < strlen(desiredSuffix); j++){
                         putchar(desiredSuffix[j]);
                     }
                     putchar('\n'); //output a newline after each word for each letter of that word
@@ -296,3 +289,4 @@ void tail(char * aString, int whichWord, char * desiredSuffix) {
         p++; //move pointer to next char in the string
     }
 }
+
